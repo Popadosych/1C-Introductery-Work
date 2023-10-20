b934b367aeb5aa409d1651a82f5de1c7ebb94127
Popados <sukhov.me@phystech.edu>
2023-10-20 22:51:04 +0300

    Added realization without ReadMe

--- main.cpp
+++ main.cpp
@@ -2,43 +2,43 @@
 #include <fstream>
 #include <filesystem>
 #include <vector>
-#include <unordered_map>
+#include "StringSimilarityCalculator.hpp"
 
 namespace TestConstants {
   const std::string kFirstPath =  "/home/popados/CLionProjects/1C-Introductery-Work/test/first_dir";
   const std::string kSecondPath = "/home/popados/CLionProjects/1C-Introductery-Work/test/second_dir";
 };
 
-static const size_t kSubstrLength = 8;
-
-
-void MakeMap(const std::string& str, std::unordered_map<std::string, int> cnt_substr) {
-    for (int i = 0; i + kSubstrLength < str.size(); ++i) {
-        std::string substring = str.substr(i, i + kSubstrLength);
-        ++cnt_substr[substring];
-    }
-}
-
-size_t Similirary(const std::string& first_str, const std::string& second_str) {
-    std::unordered_map<std::string, int> first_cnt_substr;
-    std::unordered_map<std::string, int> second_cnt_substr;
-    MakeMap(first_str, first_cnt_substr);
-    MakeMap(second_str, second_cnt_substr);
-
-    size_t cnt = 0;
-    for (auto [key, value]: first_cnt_substr) {
-        if (second_cnt_substr.find(key) != second_cnt_substr.end()) {
-            cnt += std::min(value, second_cnt_substr[key]);
+std::string MakeFileName(std::string& file_path) {
+    bool is_second_slash = false;
+    int index;
+    for (int i = file_path.size() - 1; i >= 0; --i) {
+        if (file_path[i] == '/') {
+            if (!is_second_slash) {
+                is_second_slash = true;
+            } else {
+                index = i + 1;
+                break;
+            }
         }
     }
+    return file_path.substr(index);
+}
 
-    size_t res = (std::max(first_str.size(), second_str.size()) * kSubstrLength * 100) / cnt;
-    return res;
+std::string get_string(std::ifstream& in) {
+    std::string str;
+    char sym;
+    in.get(sym);
+
+    while (!in.eof()) {
+        str += sym;
+        in.get(sym);
+    }
+    return str;
 }
 
 
-
-std::vector<std::string> Process(const std::string& first_dir_path, const std::string& second_dir_path) {
+std::vector<std::string> Process(const std::string& first_dir_path, const std::string& second_dir_path, int percentage) {
     std::vector<std::string> ans;
     const std::filesystem::path first_dir{first_dir_path};
     const std::filesystem::path second_dir{second_dir_path};
@@ -46,44 +46,50 @@ std::vector<std::string> Process(const std::string& first_dir_path, const std::s
     std::ifstream second_file;
 
     for (auto& first_iterator : std::filesystem::directory_iterator(first_dir)) {
-        first_file.open(first_iterator.path().string());
-        size_t last_slash = first_iterator.path().string().rfind("/");
-        std::string first_name = first_iterator.path().string().substr(last_slash + 1);
-        std::string first_str;
-        char sym;
-        first_file.get(sym);
-
-        while (!first_file.eof()) {
-            first_str += sym;
-            first_file.get(sym);
-        }
+        std::string first_path = first_iterator.path().string();
+        first_file.open(first_path);
+        std::string first_str = get_string(first_file);
 
         for (auto& second_iterator : std::filesystem::directory_iterator(second_dir)) {
-            last_slash = second_iterator.path().string().rfind("/");
-            std::string second_name = second_iterator.path().string().substr(last_slash + 1);
-            second_file.open(second_iterator.path().string());
-            std::string second_str;
+            std::string second_path = second_iterator.path().string();
+            second_file.open(second_path);
+            std::string second_str = get_string(second_file);
 
-            char ch;
-            second_file.get(ch);
-
-            while (!second_file.eof()) {
-                second_str += ch;
-                second_file.get(ch);
-            }
             std::string review;
-            if (first_str == second_str) {
-                review = first_name + " is identical with " + second_name;
+            size_t similarity = StringSimilarityCalculator(first_str, second_str);
+
+            std::string first_name = MakeFileName(first_path);
+            std::string second_name = MakeFileName(second_path);
+
+           if (similarity >= percentage) {
+                review += first_name;
+                review += " - ";
+                review += second_name;
+                if (similarity != 100) {
+                    review += " - ";
+                    review += std::to_string(similarity);
+                }
                 ans.push_back(review);
-                std::cout << review << '\n';
             }
+
+            second_file.close();
         }
+        first_file.close();
     }
     return ans;
 }
 
-int main() {
-    Process(TestConstants::kFirstPath, TestConstants::kSecondPath);
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        return 1;
+    }
+
+    auto ans = Process(argv[1], argv[2], atoi(argv[3]));
+
+    for (int i = 0; i < ans.size(); ++i) {
+        std::cout << ans[i] << '\n';
+    }
+
     return 0;
 }
 
